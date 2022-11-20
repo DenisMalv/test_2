2091223fa7751a88d1990983e0da16f006efe963
DenisMalv <kissed03@gmail.com>
2022-11-21 01:27:31 +0200

    //

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import "./App.css";
 import { Routes, Route } from 'react-router-dom';
 
 import Layout from "./layout/Layout";
-import Hero from "./components/Hero/Hero";
-import Expectations from "./components/Expections/Expectations";
-import Skills from "./components/Skills/Skills";
-import Expirience from "./components/Expirience/Expirience";
-import Projects from "./components/Projects/Projects";
-import Contacts from "./components/Contacts/Contacts";
+// import Hero from "./components/Hero/Hero";
+// import Expectations from "./components/Expections/Expectations";
+// import Skills from "./components/Skills/Skills";
+// import Expirience from "./components/Expirience/Expirience";
+// import Projects from "./components/Projects/Projects";
+// import Contacts from "./components/Contacts/Contacts";
 import Page404 from "./components/Page404.jsx/Page404";
 
-import img from './utils/img_imports'
+// import img from './utils/img_imports'
 
 
 function App() {
